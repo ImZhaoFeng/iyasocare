97e277b261af1090e292e777b8045f5b898b34cb
ImZhaoFeng <787250222@qq.com>
2025-07-14 11:35:36 +0800

    修改

--- js/zf-js.js
+++ js/zf-js.js
@@ -1,24 +1,26 @@
-// 加载页脚 s
-	// 原生js
-if (document.getElementById('zf-pro-footer-js')) {
-	fetch('../footer.html')
-		.then(response => response.text())
-		.then(html => {
-			document.getElementById('zf-pro-footer-js').innerHTML = html;
-		})
-		.catch(() => {});
-		// .catch(err => console.error('导航加载失败:', err));
-} else if (document.getElementById('zf-footer-js')) {
-	console.log('footer load')
-	fetch('footer.html')
-		.then(response => response.text())
-		.then(html => {
-			document.getElementById('zf-footer-js').innerHTML = html;
-		})
-		.catch(() => {});
-		// .catch(err => console.error('导航加载失败:', err));
-}
-// 加载页脚 e
+// // 加载页脚 s
+// 	// 原生js
+// if (document.getElementById('zf-pro-footer-js')) {
+// 	fetch('../footer.html')
+// 		.then(response => response.text())
+// 		.then(html => {
+// 			document.getElementById('zf-pro-footer-js').innerHTML = html;
+// 		})
+// 		.catch(() => {});
+// 		// .catch(err => console.error('导航加载失败:', err));
+// } else if (document.getElementById('zf-footer-js')) {
+// 	console.log('footer load');
+// 	fetch('footer.html')
+// 		.then(response => response.text())
+// 		.then(html => {
+// 			document.getElementById('zf-footer-js').innerHTML = html;
+// 		})
+// 		.catch(() => {});
+// 		// .catch(err => console.error('导航加载失败:', err));
+// }
+// // 加载页脚 e
+
+
 
 
 $(function(){
